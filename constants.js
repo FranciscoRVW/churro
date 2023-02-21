595fd5575a4ab3c5f07c1c41a64b2ee132812b5e
Francisco Ramirez <francisco.rv@wizeline.com>
2023-02-20 18:23:50 -0600

    Add time constants for Spain and Vietnam time

--- constants.js
+++ constants.js
@@ -7,3 +7,11 @@ const EMAIL_SENT_COL = "Email Sent";
 const ACTIVE_USER_EMAIL = Session.getActiveUser().getEmail();
 const SENDER_ALIAS_COL = "Send as"
 const HIRE_DATE_COL = "Hire date"
+
+
+/**
+ * Time zone constants
+ * All time bases calculations are located on the GMT-6 TZ
+ */
+const VIETNAM_TIME = timeNow(13)
+const SPAIN_TIME = timeNow(7)
